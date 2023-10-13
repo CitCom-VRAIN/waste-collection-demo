3aeaf194f18d7fc44385a08cac9fe37080603202
Joan Ciprià <joancipria@protonmail.com>
2023-10-13 09:33:14 +0200

    Fix LD headers & params

--- services/BrokerLD.py
+++ services/BrokerLD.py
@@ -13,12 +13,14 @@ class BrokerLD:
         protocol = os.environ.get("PROTOCOL")
         endpoint_cb = os.environ.get("ENDPOINT_CB")
 
-        url = "http://localhost:3000/ngsi-ld/v1/entities?type=WasteContainer"
+        url = "http://localhost:3000/ngsi-ld/v1/entities?type={TYPE}&scopeQ={SUBSERVICE}".format(
+            TYPE=type,
+            SUBSERVICE=subservice,
+        )
 
         payload = {}
         headers = {
             "NGSILD-Tenant": service,  # equals to "Fiware-Service": service,
-            "NGSILD-Path": subservice,  # equals to "Fiware-ServicePath": subservice,
             "X-Auth-Token": token,
             "Link": '<https://raw.githubusercontent.com/smart-data-models/dataModel.WasteManagement/master/context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"',
             "Accept": "application/ld+json",
