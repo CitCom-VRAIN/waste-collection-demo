3538716553df99d7aeb56f535cd49d96db9dfe33
Joan Ciprià <joancipria@protonmail.com>
2023-11-29 12:31:16 +0100

    Add get_all_entities_by_type_and_georel

--- services/Broker.py
+++ services/Broker.py
@@ -32,3 +32,41 @@ class Broker:
             "GET", url, headers=headers, data=payload, verify=False
         )
         return response
+
+    def get_all_entities_by_type_and_georel(
+        self,
+        type,
+        service,
+        subservice,
+        token,
+        georel,
+        geometry,
+        coords,
+        limit=20,
+        offset=0,
+    ):
+        protocol = os.environ.get("PROTOCOL")
+        endpoint_cb = os.environ.get("ENDPOINT_CB")
+
+        url = "{PROTOCOL}://{ENDPOINT_CB}/v2/entities?type={TYPE}&attrs=dateModified,dateCreated,*&limit={LIMIT}&offset={OFFSET}&options=count&georel={GEOREL}&geometry={GEOMETRY}&coords={COORDS}".format(
+            PROTOCOL=protocol,
+            ENDPOINT_CB=endpoint_cb,
+            TYPE=type,
+            LIMIT=limit,
+            OFFSET=offset,
+            GEOREL=georel,
+            GEOMETRY=geometry,
+            COORDS=coords,
+        )
+
+        payload = {}
+        headers = {
+            "Fiware-Service": service,
+            "Fiware-ServicePath": subservice,
+            "X-Auth-Token": token,
+        }
+
+        response = requests.request(
+            "GET", url, headers=headers, data=payload, verify=False
+        )
+        return response
