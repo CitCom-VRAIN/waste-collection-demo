1dc3172699d5137ac567ac6371bc78e3321066a8
Joan Ciprià <joancipria@protonmail.com>
2023-12-04 21:17:06 +0100

    Add WasteContainer class

---
+++ static/modules/WasteContainer.js
@@ -0,0 +1,10 @@
+import { Marker } from './Marker.js';
+
+export class WasteContainer {
+    constructor(id, fillingLevel, location) {
+        this.id = id;
+        this.fillingLevel = fillingLevel;
+        this.location = location
+        this.marker = new Marker(location, 'dumpster', 'blue', false, `<b>Level:</b> ${Math.round(fillingLevel.value * 100)}%`, null)
+    }
+}
