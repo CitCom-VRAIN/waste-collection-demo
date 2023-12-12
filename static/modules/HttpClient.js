7ab90078348c80d6ac7b1d308e63ceb6352315db
Joan Ciprià <joancipria@protonmail.com>
2023-12-12 13:58:14 +0100

    Add params

--- static/modules/HttpClient.js
+++ static/modules/HttpClient.js
@@ -1,10 +1,16 @@
 export class HttpClient {
     #serverURL = "http://127.0.0.1:5000/";
 
-    async get(endpoint) {
+    async get(path, params = undefined) {
+        // Build URL
+        const url = new URL(this.#serverURL);
+        url.pathname = path;
+
+        const searchParams = new URLSearchParams(params);
+
         try {
-            const request = await fetch(`${this.#serverURL}${endpoint}`)
-            const response = await request.text()
+            const request = await fetch(url.toString() + '?' + searchParams.toString());
+            const response = await request.text();
             const data = JSON.parse(response);
             return data;
         } catch (error) {
