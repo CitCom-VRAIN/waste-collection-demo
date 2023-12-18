67812334e58b6c1fe7d35487e89cde8a7530a3f5
Joan Ciprià <joancipria@protonmail.com>
2023-12-18 11:16:20 +0100

    Improve error handling and controll timeout

--- static/modules/HttpClient.js
+++ static/modules/HttpClient.js
@@ -1,7 +1,9 @@
 export class HttpClient {
     #serverURL = "http://127.0.0.1:5000/";
+    #networkTimeOut = 7000; // 7s to abort
 
-    async get(path, params = undefined) {
+    async get(path, params) {
+
         // Build URL
         const url = new URL(this.#serverURL);
         url.pathname = path;
@@ -9,13 +11,25 @@ export class HttpClient {
         const searchParams = new URLSearchParams(params);
 
         try {
-            const request = await fetch(url.toString() + '?' + searchParams.toString());
-            const response = await request.text();
-            const data = JSON.parse(response);
-            return data;
+            // Create abort controller to handle timeout
+            const controller = new AbortController();
+            const timeoutId = setTimeout(() => controller.abort(), this.#networkTimeOut);
+
+            // Fetch with timeout
+            const response = await fetch(url.toString() + '?' + searchParams.toString(), { signal: controller.signal });
+            clearTimeout(timeoutId);
+
+            if (response.status >= 200 && response.status <= 299) {
+                const jsonResponse = await response.json();
+                return jsonResponse;
+            } else {
+                // Handle errors
+                console.error(response.status, response.statusText);
+                return { error: response.statusText }
+            }
         } catch (error) {
-            console.error(error)
-            return { error: error };
+            // Timeout error
+            return { error: "Request timeout" }
         }
     }
 }
