d6cb135c052e589c6c51353ec9f6d858188048ea
Joan Ciprià <joancipria@protonmail.com>
2024-02-07 11:08:59 +0100

    Get trucks from orion

--- server.py
+++ server.py
@@ -13,6 +13,17 @@ app = Flask(__name__)
 CORS(app)
 
 
+service = ""
+subservice = ""
+
+# Authenticate
+#auth = Auth()
+#token = auth.get_auth_token_subservice(service, subservice)
+token = ""
+
+# New Broker NGSI-LD connection
+broker = Client()
+
 # Serve frontend
 @app.route("/")
 def home():
@@ -34,12 +45,6 @@ def trucks():
     return trucks
 
 
-@app.route("/districts")
-def disctricts():
-    districts = get_districts()
-    return districts
-
-
 @app.route("/optimization")
 def optimization():
     coords = json.loads(request.args.get("coords"))
@@ -52,81 +57,20 @@ def optimization():
 
 # Data functions
 def get_all_WasteContainers():
-    # Define service & subservice
-    service = "tef_vlci"
-    subservice = "/residuos_contenedores_vlc"
-
-    # Authenticate
-    auth = Auth()
-    token = auth.get_auth_token_subservice(service, subservice)
-
-    # New Broker NGSI-LD connection
-    broker = Client()
-
     # Fetch all WasteContainer entities
     limit = 400  # Entites per page (TODO: Implement pagination with response.headers["fiware-total-count"] & offset)
-    data = broker.get_all_entities_by_type(
+    containers = broker.get_all_entities_by_type(
         "WasteContainer", service, subservice, token, limit
     ).json()
 
-    return data
+    return containers
 
 
 def get_trucks():
-    # Fake data
-    vehicles = [
-        {
-            "id": "urn:ngsi-ld:VehicleModel:vehiclemodel:econic",
-            "location": {
-                "observedAt": "2023-11-16T23:01:12.111Z",
-                "type": "GeoProperty",
-                "value": {
-                    "coordinates": [-0.5045224463629362, 39.467567014494584],
-                    "type": "Point",
-                },
-            },
-            "type": "VehicleModel",
-            "brandName": "Mercedes Benz",
-            "cargoVolume": 1000,
-            "fuelType": "diesel",
-            "manufacturerName": "Daimler",
-            "modelName": "Econic",
-            "name": "MBenz-Econic2014",
-            "vehicleType": "lorry",
-            "@context": [
-                "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld",
-                "https://raw.githubusercontent.com/smart-data-models/dataModel.Transportation/master/context.jsonld",
-            ],
-        },
-        {
-            "id": "urn:ngsi-ld:VehicleModel:vehiclemodel:econic",
-            "location": {
-                "observedAt": "2023-11-16T23:01:12.111Z",
-                "type": "GeoProperty",
-                "value": {
-                    "coordinates": [-0.5052030675609576, 39.46955099682593],
-                    "type": "Point",
-                },
-            },
-            "type": "VehicleModel",
-            "brandName": "Mercedes Benz",
-            "cargoVolume": 1000,
-            "fuelType": "diesel",
-            "manufacturerName": "Daimler",
-            "modelName": "Econic",
-            "name": "MBenz-Econic2014",
-            "vehicleType": "lorry",
-            "@context": [
-                "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld",
-                "https://raw.githubusercontent.com/smart-data-models/dataModel.Transportation/master/context.jsonld",
-            ],
-        },
-    ]
+    # Fetch all WasteContainer entities
+    limit = 10  # Entites per page (TODO: Implement pagination with response.headers["fiware-total-count"] & offset)
+    trucks = broker.get_all_entities_by_type(
+        "VehicleModel", service, subservice, token, limit
+    ).json()
 
-    return vehicles
-
-def get_districts():
-    districtsURL = os.environ.get("DISTRICTS_API")
-    response = requests.get(districtsURL)
-    districts = response.json()["results"]
-    return districts
+    return trucks
